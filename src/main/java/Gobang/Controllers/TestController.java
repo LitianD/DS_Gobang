6f29a2af9b318698018f091d63fb1ba9c4ce533a
kingsun <33143208+kingsunfather@users.noreply.github.com>
2018-05-21 19:54:38 +0800

    同一了黑棋和白棋的像素大小为34px
    相对棋盘下子，保证若移动棋盘，落子位置依旧准确，棋子大小为34px*34px，棋盘每个格子为43px * 43px

--- src/main/java/Gobang/Controllers/TestController.java
+++ src/main/java/Gobang/Controllers/TestController.java
@@ -30,6 +30,7 @@ public class TestController {
     private void initialize()
     {
 
+       // boardImage.setVisible(false);
         boardImage.addEventHandler(MouseEvent.MOUSE_CLICKED,new PutEvent());
         //boardImage.setVisible(false);
 
@@ -53,23 +54,45 @@ public class TestController {
     public void setMainApplication(Main mainApplication, AnchorPane anchorPane){
         this.mainApplication=mainApplication;
         this.anchorPane=anchorPane;
-               ImageView qizi=new ImageView(new Image("images/黑棋.png"));
-        double begin_x=boardImage.getLayoutX();
-        begin_x=begin_x-22;  //
-        double begin_y=boardImage.getLayoutY();
-        begin_y=begin_y-15;  //
+
+        //相对棋盘位置来下棋子，保证后期若由于界面调整棋盘移动后，棋子落子位置依旧准确
+        ImageView qizi=new ImageView(new Image("images/黑棋.png"));
+        double begin_x=boardImage.getLayoutX()+10;
+        double begin_y=boardImage.getLayoutY()+11;
+
         qizi.setLayoutX(begin_x);
         qizi.setLayoutY(begin_y);
 
+        System.out.println(begin_x+" "+begin_y);
+
+
+        //每个棋子的像素是34px * 34px ，棋盘格子是43 * 43 px
         ImageView qizi_test=new ImageView(new Image("images/黑棋.png"));
-
         begin_x=begin_x+43;
-
         begin_y=begin_y+43;
         qizi_test.setLayoutX(begin_x);
         qizi_test.setLayoutY(begin_y);
-        //anchorPane.getChildren().add(qizi);
+
+        System.out.println(begin_x+" "+begin_y);
+
+        ImageView qizi_test2=new ImageView(new Image("images/白棋.png"));
+        begin_x=begin_x+43;
+        begin_y=begin_y+43;
+        qizi_test2.setLayoutX(begin_x);
+        qizi_test2.setLayoutY(begin_y);
+
+        System.out.println(begin_x+" "+begin_y);
+
+        ImageView qizi_test3=new ImageView(new Image("images/黑棋.png"));
+        begin_x=begin_x+43;
+        begin_y=begin_y+43;
+        qizi_test3.setLayoutX(begin_x);
+        qizi_test3.setLayoutY(begin_y);
+
+        anchorPane.getChildren().add(qizi);
         anchorPane.getChildren().add(qizi_test);
+        anchorPane.getChildren().add(qizi_test2);
+        anchorPane.getChildren().add(qizi_test3);
 
     }
 
